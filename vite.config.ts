a6d33c448c8ddf08a2f2c5fc58119909790cb9df
kavishkh <kavishkhanna06@gmail.com>
2025-09-09 00:05:23 +0530

    Merged unrelated histories

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,23 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    open: true,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  }
 }));
